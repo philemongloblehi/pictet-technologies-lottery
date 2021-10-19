c112578024dc90f4cdd52862c1a0c8b73a5d40ce
Philemon GLOBLEHI <philemon.globlehi@gmail.com>
2021-10-19 11:36:53 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * @author Philémon Globléhi <philemon.globlehi@gmail.com>
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
